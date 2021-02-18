259202e68349cec49d963fe3731ba43df9fbba06
Pivikua <pivikua@gmail.com>
2021-02-18 23:34:22 +0200

    rec

--- src/main/java/ua/pivik/testCB/GreetingController.java
+++ src/main/java/ua/pivik/testCB/GreetingController.java
@@ -21,15 +21,6 @@ public class GreetingController {
     @Autowired
     private RecipeRepo recipeRepo;
 
-/*    @GetMapping("/greeting")
-    public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Map<String, Object> model
-    ) {
-        model.put("name", name);
-        return "greeting";
-    }*/
-
     @GetMapping
     public String main(Map<String, Object> model) {
         Iterable<Recipe> recipes = recipeRepo.findAll();
@@ -45,7 +36,6 @@ public class GreetingController {
 
         Iterable<Recipe> recipes = recipeRepo.findAll();
         model.put("recipe", recipes);
-
         return "main";
     }
 }
